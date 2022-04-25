e7c33fb3ff34ac404069f8bc6c4c42bde38cf63a
Mas12344 <grzybek2002@gmail.com>
2022-04-25 15:01:07 +0200

    progress

--- Snake_pnc/main.c
+++ Snake_pnc/main.c
@@ -1,19 +1,94 @@
 #include <stdio.h>
 #include <SDL.h>
+#include <stdbool.h>
+
+SDL_Window* window;
+SDL_Renderer* renderer;
+
+
+bool init() {
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		fprintf(stderr, "Error init SDL: %s\n", SDL_GetError());
+		return false;
+	}
+
+	window = SDL_CreateWindow("Snake pnc", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+	if (!window) {
+		fprintf(stderr, "Error window create: %s\n", SDL_GetError());
+		return false;
+	}
+
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (!renderer) {
+		fprintf(stderr, "Error renderer create: %s\n", SDL_GetError());
+		return false;
+	}
+
+	return true;
+}
+
+void quit() {
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	renderer = NULL;
+	window = NULL;
+
+	SDL_Quit();
+}
+//input handling
+
+void keyDownEvent(SDL_Event e) {
+
+}
+
+void keyUpEvent(SDL_Event e) {
+
+}
+
 
 int main() {
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window* window = NULL;
-	window = SDL_CreateWindow("Snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
-	if (window == NULL) {
-		printf("Error on window creation: %s\n", SDL_GetError());
-		SDL_Quit();
+	if (!init()) {
 		return -1;
 	}
-	SDL_UpdateWindowSurface(window);
-	SDL_Delay(2000);
-	SDL_DestroyWindow(window);
 
+	bool running = true;
+	while (running) {
+		Uint64 start = SDL_GetPerformanceCounter();
+		SDL_Event e;
+		SDL_SetRenderDrawColor(renderer, 188, 188, 188, 255);
+		SDL_RenderClear(renderer);
+		/*============================
+		event loop
+
+		physics loop
+
+		rendering loop
+		=============================*/
+
+		//event loop
+		while (SDL_PollEvent(&e) != 0) {
+			switch (e.type) {
+			case SDL_QUIT:
+				running = false;
+				break;
+			case SDL_KEYDOWN:
+				keyDownEvent(e);
+				break;
+			case SDL_KEYUP:
+				keyUpEvent(e);
+			}
+		}
+
+
+		
+		SDL_RenderPresent(renderer);
+
+		Uint64 end = SDL_GetPerformanceCounter();
+
+		float elapsedTime = (end - start) / (float)SDL_GetPerformanceFrequency() * 1000.0f;
+		SDL_Delay(floor(16.666f - elapsedTime));
+	}
 	
+	quit();
 	return 0;
 }
